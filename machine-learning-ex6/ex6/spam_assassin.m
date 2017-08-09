c41a577e44cbee3faf1e8e0404b474419f5430ad
Dang Nguyen Anh Khoa <wakandan@gmail.com>
2017-08-09 22:08:51 +0800

    wip

--- machine-learning-ex6/ex6/spam_assassin.m
+++ machine-learning-ex6/ex6/spam_assassin.m
@@ -1,19 +1,17 @@
 clear; close all; clc
 
 files = glob('/home/akai/projects/learning/machine-learning-ex6/ex6/easy_ham/*.*');
-printf("%d", numel(files));
+fprintf('%d', numel(files));
+X = [];
+y = [];
 for i=1:numel(files)
+  fprintf('loading email %d\n', i);
   [~, name] = fileparts(files{i});
   file = fileread(files{i});
-  lines = strsplit(file, '\n');
-  disp(lines);
-  for line=lines
-    printf("%s", line);
-    //read until gets the header From:
-    if(regexp(line, '^From: .*<.*@.*>$'))
-      //start stripping other headers
-      
-    end
-  end
-  break;
+  content = processEmail(file);
+  features = emailFeatures(content);
+  X = [X; features'];
+  y = [y; 0];
 end
+size(X)
+size(y)
