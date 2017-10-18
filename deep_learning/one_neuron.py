1b87645f64ccb52f83044b6e49912cbb1c4561f3
Dang Nguyen Anh Khoa <kdang@axon.com>
2017-10-18 09:43:20 +0700

    try using numpy

--- deep_learning/one_neuron.py
+++ deep_learning/one_neuron.py
@@ -1,5 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import math
+import random
+from pprint import pprint
+import matplotlib
 # train a neuron to output 0 when input is 1
 #### Miscellaneous functions
 def sigmoid(z):
@@ -10,28 +13,61 @@ def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
 
-w = 2
-b = 2
+def label(x):
+    return x*0.5+10
+
+def generate_values():
+    xs = []
+    ys = []
+    for x in xrange(-50, 51):
+        #x = random.uniform(-5, 5) 
+        xs.append(x)
+        ys.append(label(x))
+    return xs, ys
+
+def normalize(xs):
+    result = []
+    min_value = min(xs)
+    max_value = max(xs)
+    for x in xs:
+        result.append((x-min_value)*1.0/(max_value-min_value))
+    return min_value, max_value, result
+
+def denormalize(x, minx, maxx):
+    return x * (maxx - minx) + minx
+
+def f(x, weights):
+    return np.dot(weights, np.array([x**3, x**2, x, 1]).transpose() )
+
+
+xs, ys = generate_values()
+minx, maxx, xs = normalize(xs)
+xs = np.asarray(xs)
+weights = np.array([4, 5, 10, -10])
+max_epoc = 5*1e1
+learning_rate = 1.2*1.e-4
 epoc = 0
-max_epoc = 100000
-x = 1
-y = 0
-learning_rate = 0.15
-outputs = []
 while epoc < max_epoc:
-    z = w*x + b
-    a = sigmoid(z)
-    cost = a - y
-    error = cost * sigmoid_prime(z)
-    gradient_b = error
-    gradient_w = error * a
-    b = b - gradient_b * learning_rate
-    w = w - gradient_w * learning_rate
-    output = sigmoid(w*x + b)
-    outputs.append(cost)
-    print('epoc: %3s, b = %s, w = %s, cost = %s => output = %s' % (epoc, b, w, cost, output))
+    gradient = np.zeros(4)
+    cost = 0
+    a = np.zeros([len(xs), 4])
+    for i in range(4):
+        a[:, i] = xs**(3-i)
+    error = np.dot(a, weights) - ys
+    error = sum(error)
+    print error
+    import sys; sys.exit()
+    for x, y in zip(xs, ys):
+        error = f(x, weights) - y
+        cost += error * error
+        derivatives = np.array([x**3, x**2, x, 1])
+        gradient += error/100.0 * derivatives
+    weights = weights - gradient * learning_rate
+    for var in 'epoc cost gradient weights'.split():
+       print "%s = %3s; " % (var, vars()[var]), 
+    print 
     epoc += 1
-print x*w+b
-print len(range(max_epoc))
-plt.plot(range(max_epoc), outputs)
-plt.show()
+x1 = -1
+x1p = (x1 - minx)*1.0/(maxx-minx)
+y1 = f(x1, weights)
+print 'test output = %s, expected = %s, error = %s' % (y1, label(x1), y1-label(x1))
