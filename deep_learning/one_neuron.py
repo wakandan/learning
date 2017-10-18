ade07b44c7fcdae8519c3560bd6cee86c3a4d321
Dang Nguyen Anh Khoa <kdang@axon.com>
2017-10-18 11:05:20 +0700

    using sklearn to normalize data

--- deep_learning/one_neuron.py
+++ deep_learning/one_neuron.py
@@ -2,7 +2,12 @@ import numpy as np
 import math
 import random
 from pprint import pprint
-import matplotlib
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+
 # train a neuron to output 0 when input is 1
 #### Miscellaneous functions
 def sigmoid(z):
@@ -15,6 +20,9 @@ def sigmoid_prime(z):
 
 def label(x):
     return x*0.5+10
+
+def transform(x):
+    return x*0.1
 
 def generate_values():
     xs = []
@@ -36,38 +44,45 @@ def normalize(xs):
 def denormalize(x, minx, maxx):
     return x * (maxx - minx) + minx
 
-def f(x, weights):
-    return np.dot(weights, np.array([x**3, x**2, x, 1]).transpose() )
+def f(x, a, b, c, d):
+    return a*x**3 + b*x**2 + c*x + d
 
-
-xs, ys = generate_values()
-minx, maxx, xs = normalize(xs)
-xs = np.asarray(xs)
-weights = np.array([4, 5, 10, -10])
-max_epoc = 5*1e1
-learning_rate = 1.2*1.e-4
+scaler = StandardScaler()
+xs_dat, ys = generate_values()
+xs_dat = np.asarray(xs_dat)
+xs_dat = xs_dat.reshape(-1, 1)
+xs = scaler.fit_transform(xs_dat)
+#minx, maxx, xs = normalize(xs)
+a = 2 
+b = 3
+c = 10
+d= -10
+max_epoc = 1e3
+learning_rate = 6*1.e-4
 epoc = 0
 while epoc < max_epoc:
-    gradient = np.zeros(4)
+    gradient_a = gradient_b = gradient_c = gradient_d = 0
     cost = 0
-    a = np.zeros([len(xs), 4])
-    for i in range(4):
-        a[:, i] = xs**(3-i)
-    error = np.dot(a, weights) - ys
-    error = sum(error)
-    print error
-    import sys; sys.exit()
     for x, y in zip(xs, ys):
-        error = f(x, weights) - y
+        error = f(x, a, b, c, d) - y
         cost += error * error
-        derivatives = np.array([x**3, x**2, x, 1])
-        gradient += error/100.0 * derivatives
-    weights = weights - gradient * learning_rate
-    for var in 'epoc cost gradient weights'.split():
+        gradient_a += error * x**3
+        gradient_b += error * x**2
+        gradient_c += error * x
+        gradient_d += error 
+    a -= gradient_a*learning_rate
+    b -= gradient_b*learning_rate
+    c -= gradient_c*learning_rate
+    d -= gradient_d*learning_rate
+    for var in 'epoc cost gradient_a a b c d'.split():
        print "%s = %3s; " % (var, vars()[var]), 
     print 
     epoc += 1
-x1 = -1
-x1p = (x1 - minx)*1.0/(maxx-minx)
-y1 = f(x1, weights)
-print 'test output = %s, expected = %s, error = %s' % (y1, label(x1), y1-label(x1))
+test_input = np.asarray([-1, 5, 10, 100])
+test_input = test_input.reshape(-1, 1)
+expected_output = label(test_input)
+scaled_test_output = scaler.transform(test_input)
+observed_output = f(scaled_test_output, a, b, c, d)
+print 'test output = %s, expected = %s, error = %s' % (expected_output, observed_output, sum(observed_output-expected_output))
+#plt.plot(xs, [label(x) for x in xs_dat], 'r-', [f(x, a, b, c, d) for x in xs])
+#plt.show()
