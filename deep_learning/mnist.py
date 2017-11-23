d50ee0f29f22c732e33da27564378018007c7e09
Dang Nguyen Anh Khoa <kdang@axon.com>
2017-11-23 20:20:52 +0700

    add test for new fully connected network

--- deep_learning/mnist.py
+++ deep_learning/mnist.py
@@ -40,11 +40,12 @@ def sigmoid_prime(z):  # sigmoid prime
 
 
 class ConvLayer:
-    def __init__(self, filter_size, fn_activation=sigmoid):
+    def __init__(self, filter_size=3, fn_activation=sigmoid, fn_derive=sigmoid_prime):
         self.filter_size = filter_size
         self.weight_array = np.random.normal(size=(filter_size, filter_size))
         self.bias = np.random.normal()
         self.fn_activation = fn_activation
+        self.fn_derive = fn_derive
 
     def forward_helper(self, inp, stride=1):
         """
@@ -76,16 +77,41 @@ class ConvLayer:
         self.input_array_size = inp.shape[0]
         return self.max_pooling(output, pool_size)
 
-    def backprop(self, delta_array, inp, stride=1):
+    def get_max_position(self, activation_array, pool_size=2):
+        array_size = activation_array.shape[0]
+        if array_size % pool_size != 0:
+            raise NotImplementedError("Array size needs to be even")
+
+        output_size = array_size / 2
+        # initialize a dummy array of max position
+        max_position_list = []
+        for i in range(array_size):
+            max_position_list.append([(j, j) for j in range(array_size)])
+        print(max_position_list)
+
+        for i in range(array_size):
+            for j in range(array_size):
+                matrix = activation_array[i / pool_size * pool_size:(i / pool_size + 1) * pool_size][:,
+                         j / pool_size * pool_size:(j / pool_size + 1) * pool_size]
+
+                row, col = np.unravel_index(matrix.argmax(), matrix.shape)
+                max_position_list[i][j] = (row + i / pool_size * pool_size, col + j / pool_size * pool_size)
+        return max_position_list
+
+    def backprop(self, delta_array, inp, stride=1, pool_size=2):
         """
-        delta_array is a (input_array_size - filter_size + 1) x 1. It should be the result of reshaping the max-pooled
+        delta_array is a (input_array_size - filter_size + 1)^2/4 x 1. It should be the result of reshaping the max-pooled
         output matrix
         :return:
         delta_array_last: a input_array_size x input_array_size
         """
         # forward step
+        delta_array_size = int(np.sqrt(delta_array.shape[0]))
+        # assert delta_array_size == self.filter_size, "delta array dimension must match with filter size"
         output_size = inp.shape[0] - self.filter_size + 1
         input_size = inp.shape[0]
+        if delta_array_size != int((input_size - self.filter_size + 1) / pool_size):
+            raise NotImplementedError("Delta size needs to be appropriate with the input size")
         weighted_input = np.zeros([output_size, output_size])
         weight_len = self.filter_size * self.filter_size
         weight = self.weight_array.reshape([1, weight_len])
@@ -96,7 +122,8 @@ class ConvLayer:
                                                   [weight_len, 1])) + \
                                        self.bias
         activation = self.fn_activation(weighted_input)
-        delta_array_size = int(np.sqrt(delta_array.shape[0]))
+        activation_size = activation.shape[0]
+        max_position_array = self.get_max_position(activation)
         delta_array = delta_array.reshape(delta_array_size, delta_array_size)
         weight_gradient_array = np.zeros((self.filter_size, self.filter_size))
         print('input size', inp.shape)
@@ -105,29 +132,59 @@ class ConvLayer:
         for a in range(self.filter_size):
             for b in range(self.filter_size):
                 weight_gradient = 0
-                for i in range(delta_array_size):
-                    for j in range(delta_array_size):
-                        # print(a, b, i, j)
-                        x = delta_array[i][j] * sigmoid_prime(weighted_input[i][j]) * inp[i + a][j + b]
+                for i in range(activation_size):
+                    for j in range(activation_size):
+                        # only calculate for the max position element
+                        if (i, j) != max_position_array[i][j]:
+                            continue
+                        x = delta_array[i / pool_size][j / pool_size] * self.fn_derive(weighted_input[i][j]) * \
+                            inp[i + a][j + b]
                         weight_gradient += x
                 weight_gradient_array[a][b] = weight_gradient
-
+        print('weight gradient array', weight_gradient_array)
         back_delta_array = np.zeros((input_size, input_size))
-        for i in range(input_size):
-            for j in range(input_size):
+        # calc backprop from the activation array instead of from the input array
+        # this should take care of the index difference problem
+        for i in range(activation_size):
+            for j in range(activation_size):
+                if (i, j) != max_position_array[i][j]:
+                    continue
                 for a in range(self.filter_size):
                     for b in range(self.filter_size):
-                        if not (0 <= i - a <= input_size - self.filter_size) or not (
-                                        0 <= j - b <= input_size - self.filter_size):
-                            continue
-                        back_delta_array[i][j] += delta_array[i - a][j - b] * sigmoid_prime(inp[i - a][j - b]) * \
-                                                  self.weight_array[a][b]
+                        back_delta_array[i + a][j + b] = delta_array[i / pool_size][b / pool_size] * \
+                                                         self.fn_derive(activation[i][j]) * self.weight_array[a][b]
+
         back_delta_array = back_delta_array / (input_size * input_size)
-        print(back_delta_array)
+        print('back delta array', back_delta_array)
         # getting average of gradient array
         weight_gradient_array = weight_gradient_array / (self.filter_size * self.filter_size)
-        print(weight_gradient_array)
+        print('weighted gradient array', weight_gradient_array)
         return weight_gradient_array, back_delta_array
+
+
+class FullyConnectedLayer:
+    def __init__(self, inp_size, out_size, fn_activation=sigmoid, fn_derive=sigmoid_prime):
+        self.inp_size = inp_size
+        self.out_size = out_size
+        self.fn_activation = fn_activation
+        self.fn_derive = fn_derive
+        self.weight_array = np.random.normal(size=(out_size, inp_size))
+        self.bias_array = np.random.normal(size=(out_size, 1))
+
+    def forward(self, inp):
+        assert inp.shape == (self.inp_size, 1), "inp.shape %s != (inp_size, 1) (%s, 1)" % (inp.shape, self.inp_size)
+        z = np.dot(self.weight_array, inp) + self.bias_array
+        a = self.fn_activation(z)
+        return a
+
+    def backprop(self, inp, delta_array):
+        assert delta_array.shape == (self.out_size, 1)
+        z = np.dot(self.weight_array, inp)
+        a = self.fn_activation(z)
+        delta_past_array = np.multiply(np.dot(self.weight_array.transpose(), delta_array), self.fn_derive(inp))
+        gradient_b = delta_array
+        gradient_w = np.dot(inp, delta_array.transpose())
+        return gradient_w, gradient_b, delta_past_array
 
 
 class Network:
--- deep_learning/mnist_test.py
+++ deep_learning/mnist_test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from mnist import ConvLayer
+from mnist import ConvLayer, FullyConnectedLayer
 
 
 class TestConvLayer(unittest.TestCase):
@@ -22,17 +22,49 @@ class TestConvLayer(unittest.TestCase):
     def test_forward(self):
         size = 10
         inp = np.random.normal(size=(size, size))
-        conv_layer = ConvLayer(filter_size=3)
+        conv_layer = ConvLayer()
         output = conv_layer.forward(inp)
         self.assertEqual(output.shape, (4, 4))
 
     def test_backprop(self):
-        size = 10
+        size = 6
         inp = np.random.normal(size=(size, size))
-        conv_layer = ConvLayer(filter_size=3)
-        output_size = size - conv_layer.filter_size + 1
+        conv_layer = ConvLayer()
+        output_size = (size - conv_layer.filter_size + 1) / 2
         delta_array_size = output_size * output_size
         delta_array = np.random.normal(size=(delta_array_size, 1))
         weight_gradient_result, past_delta_result = conv_layer.backprop(delta_array, inp)
         self.assertEqual(weight_gradient_result.shape, (conv_layer.filter_size, conv_layer.filter_size))
         self.assertEqual(past_delta_result.shape, (size, size))
+
+    def test_max_position_array(self):
+        size = 10
+        inp = np.arange(size * size).reshape(size, size)
+        conv_layer = ConvLayer()
+        out = conv_layer.get_max_position(inp)
+        self.assertEqual(len(out), 10)
+        self.assertEqual(len(out[0]), 10)
+        self.assertEqual(out[2][2], (3, 3))
+
+
+class TestFullyConnected(unittest.TestCase):
+    def test_init(self):
+        layer = FullyConnectedLayer(10, 5)
+        self.assertEqual((5, 10), layer.weight_array.shape)
+        self.assertEqual((5, 1), layer.bias_array.shape)
+
+    def test_forward(self):
+        layer = FullyConnectedLayer(10, 5)
+        inp = np.random.normal(size=(10, 1))
+        forward = layer.forward(inp)
+        print('forward, ', forward)
+        self.assertEqual((5, 1), forward.shape)
+
+    def test_backprop(self):
+        layer = FullyConnectedLayer(10, 5)
+        inp_array = np.random.normal(size=(10, 1))
+        delta_array = np.random.normal(size=(5, 1))
+        gradient_w_array, gradient_b_array, back_delta_array = layer.backprop(inp_array, delta_array)
+        self.assertEqual((10, 1), back_delta_array.shape)
+        self.assertEqual((10, 5), gradient_w_array.shape)
+        self.assertEqual((5, 1), gradient_b_array.shape)
